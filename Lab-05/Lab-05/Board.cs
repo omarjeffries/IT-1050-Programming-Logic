4c834d434cc259b269122a6ca2fbc441c794f961
Omar Jeffries <omar.jeffries@gmail.com>
2016-12-08 19:19:30 -0500

    Lab 5
    Lab 5

---
+++ Lab-05/Lab-05/Board.cs
@@ -0,0 +1,45 @@
+﻿namespace Lab_05
+{
+    class Board
+    {
+        public int Size;
+        public string[][] Grid;
+
+        public Board(int size)
+        {
+            this.Size = size;
+            this.Grid = new string[size][];
+
+            for (int i = 0; i < size; i++)
+            {
+                this.Grid[i] = new string[size];
+            }
+        }
+
+        public void Fill()
+        {
+            for (int i=0; i < 8; i++)
+            {
+                for (int j=0; j < 8; j++)
+                {
+                    this.Grid[i][j] = " X ";
+                }
+            }
+       
+        }
+
+        public void Print()
+        {
+            foreach (string[] row in Grid)
+            {
+                System.Console.WriteLine();
+
+                foreach (string stringValue in row)
+                {
+                    System.Console.Write(stringValue);
+                }
+            }
+        }
+
+    }
+}
---
+++ Lab-05/Lab-05/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Lab_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board c = new Board(8);
+            c.Fill();
+            c.Print();
+            System.Console.ReadKey();
+
+            int width = 8;
+            int height = 8;
+
+            int[,] chessSpace = new int[width, height];
+            chessSpace[0, 0] = 0;
+            
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    System.Console.Write(chessSpace [x, y] + " X ");
+                }
+                System.Console.WriteLine();
+            }
+
+            System.Console.WriteLine();
+            
+
+            while (true)
+            {
+                System.Console.WriteLine("Enter Target X Coordinate Between 0 and 7:");
+                int x = int.Parse(System.Console.ReadLine());
+
+                System.Console.WriteLine("Enter Target Y Coordinate Between 0 and 7:");
+                int y = int.Parse(System.Console.ReadLine());
+
+                System.Console.WriteLine("Enter Destination X Coordinate Between 0 and 7:");
+                int dx = int.Parse(System.Console.ReadLine());
+
+                System.Console.WriteLine("Enter Destination Y Coordinate Between 0 and 7:");
+                int dy = int.Parse(System.Console.ReadLine());
+            }
+
+
+
+        }
+    }
+}
